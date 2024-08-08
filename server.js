5868004954c8de780f1bfd44bcb6ecc96556f3a0
JefferyDiaz <silentjeff41@hotmail.com>
2024-08-08 14:18:39 -0500

    Sets Server.Js up to connection to a local Sql DB

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 const routes = require('./router')
+const sql = require("msnodesqlv8")
+
+
 
 const app = express()
 const port = 3000
 
+const connectionString = "Server=localhost;Database=master;Trusted_Connection=True;"
+const query = "SELECT * // INSITE TABLE NAME HERE"
+
+sql.query(connectionString, query, (err, rows) => {
+  console.log(rows)
+})
 
 app.use(helmet())
 app.use(cors())
